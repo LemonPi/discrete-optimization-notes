c3d4a0fad41458388681b8a1d98334f1c047f740
Johnson Zhong <johnsonzhong@hotmail.ca>
2018-07-15 11:56:23 -0400

    Solve using CSP

--- constraint/coloring/solver.py
+++ constraint/coloring/solver.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import typing
+import math
+from search import BacktrackSearch
+from csp_base import CSP
+from csp_base import Variable
+from csp_base import DifferentConstraint
 
 
 # symmetry of all colour values (since they are the same) break by always selecting the lowest available one
 # lower bound is minimum degree + 1 (eg. if there's a node that has degree 1, then we must have at least 2 colours)
 # how to deal with constraint on upper bound of colour? Is there an upper bound?
 # one upper bound is max degree d + 1
-
 
 
 class Node:
@@ -50,6 +54,67 @@ def greedy(nodes: typing.List[Node]):
     return get_output(nodes)
 
 
+def csp(nodes: typing.List[Node]):
+    def select_next_var(csp):
+        min_domain = math.inf
+        chosen_i = None
+        vs = csp.variables()
+
+        for i in range(len(vs)):
+            v = vs[i]
+            n = nodes[i]
+            if v.is_assigned():
+                continue
+            # break ties by choosing variable corresponding to node with highest degree
+            if v.domain_size() < min_domain or (
+                    v.domain_size() == min_domain and n.degree() > nodes[chosen_i].degree()):
+                chosen_i = i
+                min_domain = v.domain_size()
+
+        return (vs[chosen_i], chosen_i) if chosen_i is not None else (None, None)
+
+    def select_next_val(csp: CSP, var_i):
+        node = nodes[var_i]
+        vars = csp.variables()
+
+        # we only need to check our neighbouring colours + 1
+        neighbouring_colours = set(vars[n.id].assigned_value() for n in node.neighbours)
+        c = 0
+        while c in neighbouring_colours:
+            c += 1
+        yield c
+
+    # can use greedy algorithm as the baseline (won't need more colours than it)
+    max_colours, _, assignments = greedy(nodes)
+
+    best_colours = max_colours
+    # try to decrease the number of colours
+    for num_colours in range(max_colours - 1, 0, -1):
+        # create csp problem
+        vars = []
+        for _ in nodes:
+            vars.append(Variable(range(num_colours)))
+
+        csp = CSP(vars)
+        for n in nodes:
+            for neighbour in n.neighbours:
+                # avoid adding duplicate constraints by having all lower id != higher id
+                if n.id < neighbour.id:
+                    csp.add_constraint(DifferentConstraint(vars[n.id], vars[neighbour.id]))
+
+        # try to solve (assume last solution was the best if this time we can't solve for it)
+        bt = BacktrackSearch(csp, select_next_variable=select_next_var, select_next_value=select_next_val)
+        solution = bt.search()
+
+        if solution:
+            best_colours = num_colours
+            assignments = solution
+        else:
+            break
+
+    return best_colours, True, assignments
+
+
 def solve_it(input_data):
     # Modify this code to run your optimization algorithm
 
@@ -67,8 +132,7 @@ def solve_it(input_data):
         u, v = [int(part) for part in parts]
         nodes[u].assign_neighbour(nodes[v])
 
-    method = greedy
-    # can use greedy algorithm as the baseline (won't need more colours than it)
+    method = csp
 
     out = format_output(*method(nodes))
     return out
