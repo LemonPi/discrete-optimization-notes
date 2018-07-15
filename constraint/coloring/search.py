9c71a61105b10c5c39fa9ab81d249937de2ea8b8
Johnson Zhong <johnsonzhong@hotmail.ca>
2018-07-15 11:42:43 -0400

    Allow configuration of how to select next value

--- constraint/coloring/search.py
+++ constraint/coloring/search.py
@@ -11,21 +11,32 @@ logger = logging.getLogger(__name__)
 def get_min_domain_variable(csp):
     # by default use the fail-first principle and return the variable with the smallest domain
     min_domain = math.inf
-    var = None
-    for v in csp.variables():
+    chosen_i = None
+    vars = csp.variables()
+    for i in range(len(vars)):
+        v = vars[i]
         if not v.is_assigned() and v.domain_size() < min_domain:
             min_domain = v.domain_size()
-            var = v
+            chosen_i = i
 
-    return var
+    return (vars[chosen_i], chosen_i) if chosen_i is not None else (None, None)
+
+
+def get_value_in_domain_order(csp, var_i):
+    var = csp.variables()[var_i]
+    for val in var:
+        yield val
 
 
 class BacktrackSearch:
     def __init__(self, csp: CSP, prop=propagator.ForwardCheck(),
-                 select_next_variable: typing.Callable[[CSP], Variable] = get_min_domain_variable):
+                 select_next_variable: typing.Callable[[CSP], typing.Tuple[Variable, int]] = get_min_domain_variable,
+                 select_next_value: typing.Callable[[CSP, int], typing.Any] = get_value_in_domain_order):
+
         self._csp = csp
         self._propagator = prop
         self._get_next_var = select_next_variable
+        self._get_next_val = select_next_value
         self._solution = None
 
     def restore_all_domains(self):
@@ -61,7 +72,7 @@ class BacktrackSearch:
     def recurse(self):
         """Return whether we had a domain wipeout"""
 
-        var = self._get_next_var(self._csp)
+        var, var_i = self._get_next_var(self._csp)
 
         # done since no more variables to assign
         if var is None:
@@ -69,7 +80,7 @@ class BacktrackSearch:
             self._solution = [v.assigned_value() for v in self._csp.variables()]
             return False
 
-        for val in var:
+        for val in self._get_next_val(self._csp, var_i):
             var.assign(val)
             domain_wipeout, prunings = self._propagator(self._csp, var)
 
